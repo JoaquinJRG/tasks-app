907da0ddc07ed178577f01e81ac060694042c69a
Joaquin <joaquinjrg01@gmail.com>
2024-08-14 09:57:35 +0200

    responsive dropdown and scrollbar color

--- app/components/DropDown.jsx
+++ app/components/DropDown.jsx
@@ -60,7 +60,7 @@ export function DropDown() {
   return (
     <details className="relative w-40 lg:w-44 py-2 px-1 rounded-md shadow-sm bg-[#a688fa]">
       <summary className="flex items-center justify-center gap-1 list-none select-none cursor-pointer ">
-        <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={1} strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-sort-ascending">
+        <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={1} strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-sort-ascending hidden md:block">
           <path stroke="none" d="M0 0h24v24H0z" fill="none" />
           <path d="M4 6l7 0" />
           <path d="M4 12l7 0" />
